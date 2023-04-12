2bfa6817ee564617088533ee042aeef2477ac108
tutak1i20 <102325269+tutak1i20@users.noreply.github.com>
2023-04-12 13:33:13 +0200

    Add files via upload

---
+++ 4_04/pomoc do zadania/teachingProgram.js
@@ -0,0 +1,45 @@
+const hoursToBeCompleted = 140;
+const teachingProgram = [
+  {
+    name: 'Moduł wstępny',
+    topic: [
+      'Czynności wstępne i szkolenia, struktura i organizacja pracy informatyków',
+      'Struktura organizacyjna przedsiębiorstwa i elementy przetwarzania informacji',
+      'Konfiguracja sprzętu komputerowego i oprogramowania',
+    ],
+  },
+  {
+    name: 'Montaż, naprawa, konserwacja i obsługa sprzętu komputerowego',
+    topic: [
+      'Montaż sprzętu komputerowego',
+      'Naprawa  sprzętu komputerowego',
+      'Konserwacja i obsługa sprzętu komputerowego',
+    ],
+  },
+
+  {
+    name: 'Systemy informatyczne',
+    topic: [
+      'Zasady administrowania systemami informatycznymi i archiwizowania danych',
+      'Dokumentacja technologiczna przetwarzania informacj',
+      'Biblioteki oprogramowania i zbiorów danych',
+      'Zapoznanie z programami do administracji lokalnymi sieciami komputerowymi',
+      'Obsługa programów używanych w przedsiębiorstwie',
+      'komputerowe wspomaganie procesów projektowania',
+      'Organizacja, zbieranie i kontrola danych, przetwarzanie i wykorzystywanie wyników',
+      'Elementy procesów projektowania, programowania i uruchamiania programów komputerowych',
+      'Ochrona danych i procesów przetwarzania',
+    ],
+  },
+
+  {
+    name: 'Tworzenie aplikacji internetowych',
+    topic: [
+      'Konfiguracja serwerów i przeglądarek pod obsługę aplikacji internetowych',
+      'Struktura witryny internetowej - HTML',
+      'Stylizacja elementów witryny - CSS',
+      'Programowanie aplikacji internetowych - JS, PHP',
+      'Testowanie aplikacji internetowych',
+    ],
+  },
+];
